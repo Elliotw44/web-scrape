17d83f3294f6a9b01f00ad43cd2d1d7e0c491c18
Elliot Weil <eweil@eweil-mac3.tsi.lan>
2017-07-19 08:17:46 -0700

    added webscrape.py

---
+++ webscrape.py
@@ -0,0 +1,94 @@
+__author__ = 'eweil'
+
+
+import requests
+import bs4
+import csv
+import multiprocessing
+
+def IsListingPage(page):
+    title_text = page.find_all('title')[0].get_text()
+    mls_num_pos = title_text.find("MLS#")
+    return mls_num_pos > 0
+
+
+def GetMLSNumber(listing_page):
+    for child in listing_page.find_all('dl'):
+        for grand_child in child.children:
+            try:
+                if grand_child.get_text().find("MLS ID:") != -1:
+                    str = grand_child.get_text()
+                    num_start = str.find(':') + 1
+                    return str[num_start:].strip()
+            except AttributeError:
+                continue
+
+
+def ParseInfoFromListingPage(listing_page, url):
+    title_text = listing_page.find_all('title')[0].get_text()
+    pipe_one = title_text.find('|')
+    address = title_text[0:pipe_one-1]
+    mls_num = GetMLSNumber(listing_page)
+    record = {'address': address, 'mls': mls_num, 'url': url}
+    return record
+
+
+def PageURlAddOn(pages_parsed):
+    if pages_parsed == 0:
+        return ""
+    else:
+        return str(pages_parsed+1) + "-pg"
+
+
+def HandleURL(url):
+    try:
+        child_link = requests.get(url)
+    except IOError:
+        raise BaseException
+    child_soup = bs4.BeautifulSoup(child_link.content, 'html.parser')
+    if not IsListingPage(child_soup):
+        raise BaseException
+    else:
+        return ParseInfoFromListingPage(child_soup, url)
+
+
+def main():
+    number_of_pages = int(raw_input("please enter the number of pages to parse: "))
+    pages_parsed = 0
+    base_url = "http://jamesonsothebys.com"
+    base_page_url = 'http://jamesonsothebys.com/eng/sales/usa/'
+    urls = []
+    records = []
+    while pages_parsed < number_of_pages:
+        page = requests.get(base_page_url + PageURlAddOn(pages_parsed))
+        soup = bs4.BeautifulSoup(page.content, 'html.parser')
+        for link in soup.find_all('a'):
+            if 'href' in link.attrs and link.attrs['href'].find('/eng/sales/detail/') == 0:
+                url = (base_url + (link.attrs['href']))
+                if url in urls:
+                    continue
+                urls.append(base_url + link.attrs['href'])
+        pages_parsed += 1
+
+    output = multiprocessing.Queue()
+    for url in urls:
+        try:
+            url_info = HandleURL(url)
+            records.append(url_info)
+        except BaseException:
+            continue
+
+    with open('url_data.csv', 'w') as csvfile:
+        field_names = ["mls", "address", "url"]
+        datawriter = csv.DictWriter(csvfile, fieldnames=field_names)
+        datawriter.writeheader()
+        for record in records:
+            datawriter.writerow({"mls": record['mls'], "address": record['address'], "url": record['url']})
+
+
+
+
+
+main()
+
+
